2ae27c72f3ccdb60384f76535521450c9dcf9422
Ivan Rozhchenko <rozhchenkoivan@gmail.com>
2026-02-21 17:10:42 +0200

    config store, add transactionSlice, transactionThunks with fetchTransactions and addTransaction thunks

---
+++ src/features/store/asyncThunks/transactionsThunks.ts
@@ -0,0 +1,21 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import type { Transaction } from "../../../types/transaction.type";
+
+const apiURL = import.meta.env.VITE_SERVER_URL;
+
+export const fetchTransactions = createAsyncThunk(
+  "transactions/fetchTransactions",
+  async (userId: number) => {
+    const response = await axios.get(`${apiURL}/transactions?userId=${userId}`);
+    return response.data;
+  },
+);
+
+export const addTransaction = createAsyncThunk(
+  "transactions/addTransaction",
+  async (transactionData: Transaction) => {
+    const response = await axios.post(`${apiURL}/transactions`, transactionData);
+    return response.data;
+  },
+);
---
+++ src/features/store/slices/transactionsSlice.ts
@@ -0,0 +1,59 @@
+import type { Transaction } from "../../../types/transaction.type";
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import {
+  addTransaction,
+  fetchTransactions,
+} from "../asyncThunks/transactionsThunks";
+
+interface TransactionsState {
+  transactions: Transaction[];
+  loading: boolean;
+  error: string | null;
+}
+
+const initialState: TransactionsState = {
+  transactions: [],
+  loading: false,
+  error: null,
+};
+
+const transactionsSlice = createSlice({
+  name: "transactions",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchTransactions.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(
+        fetchTransactions.fulfilled,
+        (state, action: PayloadAction<Transaction[]>) => {
+          state.loading = false;
+          state.transactions = action.payload;
+        },
+      )
+      .addCase(fetchTransactions.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error?.message || "Failed to fetch transactions";
+      })
+      .addCase(addTransaction.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(
+        addTransaction.fulfilled,
+        (state, action: PayloadAction<Transaction>) => {
+          state.loading = false;
+          state.transactions.push(action.payload);
+        },
+      )
+      .addCase(addTransaction.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error?.message || "Failed to add transaction";
+      });
+  },
+});
+
+export default transactionsSlice.reducer;
