0a7badd497f22ade066e9918dd32361e6e5529bb
Ivan Rozhchenko <rozhchenkoivan@gmail.com>
2026-02-21 17:11:35 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
-import "./App.css";
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "./features/store/hooks";
+import {
+  fetchTransactions,
+} from "./features/store/asyncThunks/transactionsThunks";
 
 function App() {
-  return <></>;
+  const { transactions, loading, error } = useAppSelector(
+    (state) => state.transactions,
+  );
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchTransactions(1));
+  }, [dispatch]);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
+
+  return (
+    <div>
+      <ul>
+        {transactions.map((tx) => (
+          <li key={tx.id}>
+            {tx.type.toUpperCase()}: {tx.category} - ${tx.amount} on {tx.date}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
