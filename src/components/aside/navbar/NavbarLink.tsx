a71c9b8a57c41dfb198305cc2c0eecc0bd605610
Ivan Rozhchenko <rozhchenkoivan@gmail.com>
2026-03-07 21:47:20 +0200

    add styles on active link

--- src/components/aside/navbar/NavbarLink.tsx
+++ src/components/aside/navbar/NavbarLink.tsx
@@ -1,19 +1,20 @@
 import { Link } from "react-router-dom";
 import { Button } from "../../ui/button";
 
-function NavbarLink({
-  path,
-  children,
-}: {
+interface NavbarLinkProps {
   path: string;
   children: React.ReactNode;
-}) {
+}
+
+function NavbarLink({ path, children }: NavbarLinkProps) {
+  const isActive = window.location.pathname === path;
+
   return (
     <Button
       variant="ghost"
       size="xl"
       data-icon="inline-start"
-      className="justify-start w-full box-border"
+      className={`justify-start w-full box-border ${isActive ? "text-primary bg-primary/10 hover:text-primary hover:bg-primary/10" : ""}`}
       asChild
     >
       <Link to={path}>{children}</Link>
