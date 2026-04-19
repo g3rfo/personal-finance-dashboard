25ec6ba0e3837ef3aa934f35cbff03a1510a3bdc
Ivan Rozhchenko <rozhchenkoivan@gmail.com>
2026-04-19 18:17:05 +0300

    refactor navbar

--- src/components/aside/navbar/Navbar.tsx
+++ src/components/aside/navbar/Navbar.tsx
@@ -1,38 +1,12 @@
-import {
-  IconLayoutDashboard,
-  IconReportMoney,
-  IconFolder,
-  IconDeviceAnalytics,
-  IconSettings,
-} from "@tabler/icons-react";
-import NavbarLink from "./NavbarLink";
 import Outline from "../../ui/Outline";
+import NavbarContent from "./NavbarContent";
 
 function Navbar() {
   return (
     <>
       <Outline className="p-4 flex-1 items-start">
         <nav className="w-full flex flex-col gap-2">
-          <NavbarLink path="/dashboard">
-            <IconLayoutDashboard stroke={1.5} />
-            Dashboard
-          </NavbarLink>
-          <NavbarLink path="/transactions">
-            <IconReportMoney stroke={1.5} />
-            Transactions
-          </NavbarLink>
-          <NavbarLink path="/categories">
-            <IconFolder stroke={1.5} />
-            Categories
-          </NavbarLink>
-          <NavbarLink path="/analytics">
-            <IconDeviceAnalytics stroke={1.5} />
-            Analytics
-          </NavbarLink>
-          <NavbarLink path="/settings">
-            <IconSettings stroke={1.5} />
-            Settings
-          </NavbarLink>
+          <NavbarContent />
         </nav>
       </Outline>
     </>
